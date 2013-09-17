47626b6446f094c23f919275c03f1454737ddd24
Joe Bentley <joebentley10@gmail.com>
2013-09-17 13:45:54 +0100

    Started parsing

--- tasm.c
+++ tasm.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "instructs.h"
 
@@ -26,15 +28,38 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // Read from file until halt
+    // Read from file until halt, store in src
     int i = 0;
     char c;
     while ((c = fgetc(input)) != EOF) {
         src[i++] = c;
     }
 
+    // Seperate string by newlines
+    char ** res = NULL;
+    char *  p   = strtok(src, "\n");
+    int n_spaces = 0;
+    i = 0;
+
+    while (p) {
+        res = realloc(res, sizeof (char*) * ++n_spaces);
+
+        if (res == NULL)
+            return -1;
+
+        res[n_spaces-1] = p;
+
+        p = strtok(NULL, "\n");
+    }
+
+    /*// Set last element to NULL
+    res = realloc(res, sizeof (char*) * ++n_spaces);
+    res[n_spaces] = 0;*/
+
+    for (i = 0; i < (n_spaces+1); ++i)
+        printf ("res[%d] = %s\n", i, res[i]);
+
     fclose(input);
 
-    printf(src);
     return 0;
 }
